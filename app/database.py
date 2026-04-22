6897da834eb977e358e6885c20e20efe64d79eab
Lesoutias <metreyves@gmail.com>
2026-04-22 21:07:40 +0200

    Change URL of Database

--- app/database.py
+++ app/database.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 
 load_dotenv()
-DATABASE_URL = "mysql+pymysql://root:Lesoutils%401907@localhost:3306/taxe_app_db"
+DATABASE_URL = f"mysql+pymysql://{os.getenv('MYSQLUSER')}:{os.getenv('MYSQLPASSWORD')}@{os.getenv('MYSQLHOST')}:{os.getenv('MYSQLPORT')}/{os.getenv('MYSQLDATABASE')}"
+# DATABASE_URL = "mysql+pymysql://root:@localhost:3306/taxe_app_db"
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", DATABASE_URL)
 
